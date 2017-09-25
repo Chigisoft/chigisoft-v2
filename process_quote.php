40fd496f418acb9b97beb791c0e6a774befbbf8c
Okwu Joscelyn Kinikachi <ojk564@gmail.com>
2017-09-25 13:46:46 +0100

    update

--- process_quote.php
+++ process_quote.php
@@ -4,10 +4,10 @@
     $company = $_POST['company'];
     $type = $_POST['type'];
     $describe = $_POST['describe'];
-    $subject = "<h3>Project Quote Request for $company by $fullname</h3>.<br><br> we require a qoute for a $type project.<br> $describe";
+    $subject = "Quote Request for $type project";
     $to = "contact@chigisoft.com";
 
-    $txt = $describe;
+    $txt = "We require a qoute for a $type project. <br>".$describe."<br>"."$fullname <br>For: $company";
     $header = "From: $email\r\n";
     $header .= "Cc:$email \r\n";
     $header .= "MIME-Version: 1.0\r\n";
@@ -16,8 +16,9 @@
 	$retval = mail ($to,$subject,$txt,$header);
 
 	if( $retval == true ) {
-	echo "Message sent successfully...<font color='green'><i class='glyphicon glyphicon-ok'></i></font><br>We'll be in touch.";
+		echo "Message sent successfully...<font color='green'><i class='glyphicon glyphicon-ok'></i></font><br>We'll be in touch.";
 	}else {
-	echo "Message could not be sent...<font color='red'><i class='glyphicon glyphicon-warning-sign'></i></font><br>Please try again.";
+		echo "Message could not be sent...<font color='red'><i class='glyphicon glyphicon-warning-sign'></i></font><br>Please try again.";
 	}
 ?>
+
